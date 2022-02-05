9de916cc8e9f949863da2fbc99fe1414fb7e3943
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-02-05 21:36:54 +0200

    js

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ async function iegutDarglietasNoApi() {
     return datiJson
 }
 
+async function iegutElectronicsNoApi() {
+    let datiNoApi = await fetch('https://fakestoreapi.com/products/category/electronics');
+    let datiJson = await datiNoApi.json();
+    // console.log(datiJson);
+    return datiJson
+}
+
 async function iegutDarbaLaiksNoApi() {
     let datiNoApi = await fetch('https://normundsb.github.io/apiUnFetch/darbalaiks.json');
     let datiJson = await datiNoApi.json();
     // console.log(datiJson);
     return datiJson
+}
+async function raditDatus() {
+    let darglietasJson = await iegutDarglietasNoApi();
+    let electronicsJson = await iegutElectronicsNoApi();
+    console.log(darglietasJson)
 }
